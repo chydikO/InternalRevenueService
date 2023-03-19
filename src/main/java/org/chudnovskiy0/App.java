f596363b870d5e0c7440b346eca2930d5fca24a2
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2023-03-19 08:33:28 +0200

    checkout to dev

--- src/main/java/org/chudnovskiy0/App.java
+++ src/main/java/org/chudnovskiy0/App.java
@@ -3,23 +3,22 @@ package org.chudnovskiy0;
 import java.util.*;
 
 /**
- *
- Реализовать базу данных налоговой инспекции по штрафам. Идентифицировать конкретного человека бу-
- дет его персональный идентификационный код. У одного человека может быть много штрафов.
- Реализовать:
- 1. Полная распечатка базы данных.
- 2. Распечатка данных по конкретному коду.
- 3. Распечатка данных по конкретному типу штрафа.
- 4. Распечатка данных по конкретному городу.
- 5. Добавление нового человека с информацией о нем.
- 6. Добавление новых штрафов для уже существующей записи.
- 7. Удаление штрафа.
- 8. Замена информации о человеке и его штрафах.
-
+ * Реализовать базу данных налоговой инспекции по штрафам. Идентифицировать конкретного человека будет
+ * его персональный идентификационный код. У одного человека может быть много штрафов.
+ * Реализовать:
+ * 1. Полная распечатка базы данных.
+ * 2. Распечатка данных по конкретному коду.
+ * 3. Распечатка данных по конкретному типу штрафа.
+ * 4. Распечатка данных по конкретному городу.
+ * 5. Добавление нового человека с информацией о нем.
+ * 6. Добавление новых штрафов для уже существующей записи.
+ * 7. Удаление штрафа.
+ * 8. Замена информации о человеке и его штрафах.
  */
 public class App {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final Map<User, FineList> users = new TreeMap<>();
+    private static final Map<City, User> users = new HashMap<>();
+    private static final Set<City> cites = new HashSet<>();
     private static final String MENU_SEPARATOR = "-";
 
     public static void main(String[] args) {
@@ -34,6 +33,7 @@ public class App {
                 System.out.println(value.label);
             }
             try {
+                initCity();
                 option = scanner.nextInt();
                 scanner.nextLine();  // Consume newline left-over
 
@@ -54,6 +54,8 @@ public class App {
         } while (option > menu.length || option < 1);
         return option;
     }
+
+
 
     private static void getByCode() {
         System.out.println(menuTitle(Menu.GET_BY_CODE.label));
@@ -77,6 +79,42 @@ public class App {
 
     private static void addNewUser() {
         System.out.println(menuTitle(Menu.ADD_NEW_USER.label));
+        String name;
+        String soname;
+
+        do {
+            System.out.print("Введите имя:\t");
+            name = scanner.nextLine();
+            System.out.print("Введите фамилию:\t");
+            soname = scanner.nextLine();
+        } while (name.isBlank() && soname.isBlank());
+        FIO fio = new FIO(name, soname);
+
+        // enter city
+        for (City value :cites) {
+            System.out.println(value.toString());
+        }
+        City cityByIndex;
+        do {
+            System.out.print("Введите индекс города:\t");
+            final String cityIndex = scanner.nextLine().trim();
+            cityByIndex = cites.stream()
+                    .filter(city -> cityIndex.equals(Integer.toString(city.getValue())))
+                    .findAny()
+                    .orElse(null);
+        } while (cityByIndex == null);
+
+        User user = new User(fio, new HashSet<>());
+
+        //TODO: isUserPresent() by key and value
+
+        try {
+            users.put(cityByIndex, user);
+        } catch (Exception e) {
+            System.err.println(Arrays.toString(e.getStackTrace()));
+        }
+
+        //TODO: if user have fine -> add or continue;
 
         System.out.println(menuTitle(MENU_SEPARATOR.repeat(Menu.ADD_NEW_USER.label.length())));
     }
@@ -84,6 +122,7 @@ public class App {
 
     private static void UpdateRecord() {
         System.out.println(menuTitle(Menu.UPDATE_RECORD.label));
+
 
         System.out.println(menuTitle(MENU_SEPARATOR.repeat(Menu.UPDATE_RECORD.label.length())));
     }
@@ -97,17 +136,33 @@ public class App {
     private static void printUsers() {
         System.out.println(menuTitle(Menu.PRINT_DATA.label));
 
-//        for (User user : users) {
-//            System.out.println(user);
+//        for(City city : users.keySet()) {
+//            System.out.println(users.get(city) + "->" + city.getValue() + " " + city.getLabel());
 //        }
+
         System.out.println(menuTitle(MENU_SEPARATOR.repeat(Menu.PRINT_DATA.label.length())));
     }
-    
+
 
     private static void exit() {
         System.out.println("-= Выход =-");
     }
+
     private static String menuTitle(String title) {
-        return new StringBuilder().append("\t-= ").append(title).append(" =-").toString();
+        return "\t-= " + title + " =-";
+    }
+
+    private static void initCity() {
+        cites.add(new City("Черновцы", 65000));
+        cites.add(new City("Днепропетровск", 53271));
+        cites.add(new City("Ивано-Франковск", 77556));
+        cites.add(new City("Каменец-Подольский", 32300));
+        cites.add(new City("Харьков", 64341));
+        cites.add(new City("Херсон", 74830));
+        cites.add(new City("Киев", 01001));
+        cites.add(new City("Кривой Рог", 50000));
+        cites.add(new City("Львов", 79000));
+        cites.add(new City("Николаев", 54001));
+        cites.add(new City("Одесса", 65000));
     }
 }
--- src/main/java/org/chudnovskiy0/User.java
+++ src/main/java/org/chudnovskiy0/User.java
@@ -2,7 +2,10 @@ package org.chudnovskiy0;
 
 import lombok.*;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 
 @AllArgsConstructor
@@ -13,6 +16,44 @@ import java.util.List;
 
 public class User {
     private FIO fio;
-    private List<FineData> fineDataList;
+    private final int fiscalCode;
+    private Set<FineData> fineDataSet;
 
+    public User(FIO fio, HashSet<FineData> fineDataSet) {
+        this.fio = fio;
+        this.fiscalCode = generateUniqueId();
+        this.fineDataSet = fineDataSet;
+    }
+
+    public boolean addFine(FineData fine) {
+        if (fineDataSet.add(fine)) {
+            System.out.println(" Штраф " + fine + " добавлен ");
+            return true;
+        }
+        System.out.println(" Штраф " + fine + " уже добавлен");
+        return false;
+    }
+
+    public boolean deleteFine(FineData fine) {
+        if (fineDataSet.remove(fine)) {
+            System.out.println(" Штраф " + fine + " удален ");
+            return true;
+        }
+        System.out.println(" Штраф " + fine + " не существует");
+        return false;
+    }
+
+    public void printAllFineDataSet() {
+        System.out.println(" $$$$$$ Список штрафов $$$$$$");
+        fineDataSet.stream().forEach((Consumer<? super FineData>) System.out::println);
+        System.out.println(" $$$$$$");
+    }
+
+    private int generateUniqueId() {
+        UUID randomUUID = UUID.randomUUID();
+        String str = fio.getName() + fio.getSoname() + randomUUID;
+        int uid = str.hashCode();
+        str = Integer.toString(uid).replaceAll("-", "");
+        return Integer.parseInt(str);
+    }
 }
